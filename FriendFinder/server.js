a968bd0f834f9515ca6ec3060ec0a467bc6b5965
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-10-21 15:58:46 -0700

    listening server

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,15 +1,13 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
-	console.log('App listening on PORT: '+PORT);
+	console.log("App listening on PORT: "+PORT);
 });
-
-
