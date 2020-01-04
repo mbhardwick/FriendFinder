a62bc863fb3647f7059570ca652137a41643a7ee
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2020-01-03 21:32:16 -0800

    setup

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-var express = require("express");
+var express = require('express');
+var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+//import routes
+var htmlRoutes = require("./app/routing/htmlRoutes");
+var homeRoute = require('./app/routing/homeRoute');
+var friendRoute = require('./app/routing/friendsRoute');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', htmlRoutes);
+app.use('/data', friendRoute);
+app.use(homeRoute);
+
+app.use(function(req, res){
+	res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+});
 
 app.listen(PORT, function(){
 	console.log("App listening on PORT: "+PORT);
