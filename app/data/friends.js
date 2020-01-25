61f7c75d9b2440a1c3b594975d08c52472debb6c
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2020-01-25 13:46:30 -0800

    friends added

---
+++ app/data/friends.js
@@ -0,0 +1,64 @@
+var friendsArray = [
+	{
+		"name":"Rose",
+		"photo":"https://vignette.wikia.nocookie.net/goldengirls/images/9/9f/RoseNylund1.jpg/revision/latest?cb=20100305000702",
+		"scores":[
+			3,
+			2,
+			1,
+			1,
+			5,
+			1,
+			3,
+			2,
+			4,
+			5
+		]
+	}, {
+		"name": "Dorothy",
+		"photo": "https://vignette.wikia.nocookie.net/goldengirls/images/4/4e/Beaarthur_%281%29.jpg/revision/latest/top-crop/width/360/height/450?cb=20101208103602",
+		"scores": [
+			2,
+			5,
+			5,
+			3,
+			1,
+			3,
+			5,
+			4,
+			1,
+			3
+		]
+	}, {
+		"name": "Blanche",
+		"photo": "https://vignette.wikia.nocookie.net/goldengirls/images/c/cb/Blanche.jpg/revision/latest/top-crop/width/360/height/450?cb=20180117224407",
+		"scores": [
+			4,
+			3,
+			3,
+			1,
+			3,
+			3,
+			4,
+			1,
+			2,
+			5
+		]
+	}, {
+		"name": "Sophia",
+		"photo": "https://vignette.wikia.nocookie.net/goldengirls/images/9/9e/Sophia1.jpg/revision/latest/top-crop/width/360/height/450?cb=20100305000442",
+		"scores": [
+			5,
+			4,
+			4,
+			1,
+			1,
+			4,
+			5,
+			1,
+			1,
+			1
+		]
+	}
+
+]
